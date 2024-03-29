f954c824ea7e7a5c1f7d75855a8e0e2f9785abfa
Rupam800 <164736309+Rupam800@users.noreply.github.com>
2024-03-29 23:17:55 +0530

    img update

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -3,3 +3,12 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'myapp/index.html')
+
+def cart(request):
+    return render (request, 'myapp/cart.html')
+
+def menu(request):
+    return render (request, 'myapp/menu.html')
+
+def book(request):
+    return render (request, 'myapp/book.html')
