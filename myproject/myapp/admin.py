a0b18800351f2706b726b19c115a33be7748d7a4
ShovanPal <shovanpalvsk2001@gmail.com>
2024-03-30 13:28:01 +0530

    admin

--- myproject/myapp/admin.py
+++ myproject/myapp/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from . models import CustomUser
 
 # Register your models here.
+
+@admin.register(CustomUser)
+class userAdmin(admin.ModelAdmin):
+    list_display=('mobile','address')
